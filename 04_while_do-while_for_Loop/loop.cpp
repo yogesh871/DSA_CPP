b5875a670885861a0bde0b46fabdda78924de7be
yogesh <yogeshrd1708@gmail.com>
2024-12-14 18:12:01 +0530

    project-day-1

---
+++ 04_while_do-while_for_Loop/loop.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+//  while loop
+
+// int i =1;
+// while (i <= 10){
+//     cout << i << endl;
+//     i++;
+// }
+
+//  do while loop
+
+// int i = 1;
+// do{
+//       cout << i << endl;
+//     i++;
+// }
+// while (i <= 10);
+
+// for loop
+
+int i;
+for(i = 1; i<=10; i++ ){
+    cout << i << endl;
+}
+
+return 0;
+
+}
