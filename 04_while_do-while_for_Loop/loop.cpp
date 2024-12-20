d62bd5b44ba4f00ef62c7fc87cf0e261cfbf8523
yogesh <yogeshrd1708@gmail.com>
2024-12-20 11:11:00 +0530

    Loop 20/12/2024

--- 04_while_do-while_for_Loop/loop.cpp
+++ 04_while_do-while_for_Loop/loop.cpp
@@ -5,21 +5,23 @@ int main()
 {
 
 //  while loop
-
-// int i =1;
-// while (i <= 10){
-//     cout << i << endl;
-//     i++;
-// }
+{
+int i =1;
+while (i <= 10){
+    cout << i << endl;
+    i++;
+}
+}
 
 //  do while loop
-
-// int i = 1;
-// do{
-//       cout << i << endl;
-//     i++;
-// }
-// while (i <= 10);
+{
+int i = 1;
+do{
+      cout << i << endl;
+    i++;
+}
+while (i <= 10);
+}
 
 // for loop
 
--- 05_loop_Exersize/loop.cpp
+++ 05_loop_Exersize/loop.cpp
@@ -5,50 +5,46 @@ int main()
 {
     // sum
 
-    // int i, n, sum;
+{
+     int i, n, sum;
+         cout << "Enter a Number" << endl;
+         cin >> n;
+     i = 1;
+     sum = 0;
 
-    //     cout << "Enter a Number" << endl;
-    //     cin >> n;
-    // i = 1;
-    // sum = 0;
-
-    // while(i <= n){
-    //     sum = sum + i;
-    //     i++;
-    // }
-    // cout << "sum is " << sum ;
+     while(i <= n){
+         sum = sum + i;
+         i++;
+     }
+     cout << "sum is " << sum  << endl;
+}
 
     // factorial
+{
+    int i, n, fact;
+    i = 1;
+    cout << "Enter a Number" << endl;
+    cin >> n;
+    fact = 1;
+    while (1 <= n)
+    {
+        fact *= n; 
+        n--;
+    }
+    cout << "fact is " << fact << endl;
+}
 
-    // int i, n, fact;
-
-    // i = 1;
-    // cout << "Enter a Number" << endl;
-    // cin >> n;
-    // fact = 1;
-    // while (1 <= n)
-    // {
-    //     fact *= n; 
-    //     n--;
-    // }
-
-    // cout << "fact is " << fact;
-
-
-    
     // factors
 
     int i, num, n;
-
-
     cout << "Enter a Number";
-    cin << n;
+    cin >> n;
     i = 1;
 
   while (n <= i)
   {
     if(num%i == 0){
-        cout << num
+        cout << num;
     }
   }
   
