be9279ee7efd3597e86971000bdaf987447c43de
Rathod Yogesh <yogeshrd1708@gmail.com>
2024-12-21 18:17:24 +0530

    project-3.cpp. 21/12/24

--- project-cpp-3/project-3.cpp
+++ project-cpp-3/project-3.cpp
@@ -1,119 +1,112 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
-
+    // Q.1
+    cout << "Q.1" << endl;
+    for(int i = 41; i <= 45; i++)
     {
-        //  Question no. 01 
-        int i, j;
-
-        for (i = 41; i <= 45; i++)
+        for(int j = 41; j <= i; j++)
         {
-            for (j = 41; j <= i; j++)
-            {
-                cout << j << " ";
-            }
-            cout << endl;
+            cout << j << " ";
         }
+        cout << endl;
     }
 
-    
-        //  Question no. 02 =>
-  
+    // Q.2
+    cout << "Q.2" << endl;
+    int num = 11;
+    for(int i = 11; i <= 14; i++)
+    {
+        for(int j = 11; j <= i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
 
-    // int i , j, value;
+    // Q.3
+    cout << "Q.3" << endl;
+    for(int i = 5; i >= 1; i--)
+    {
+        for(int k = 5; k > i; k--)
+        {
+            cout << "  ";
+        }
+        for(int j = 1; j <= i; j++)
+        {
+            if(j % 2 == 1)
+            {
+                cout << "1 ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
 
-    //  for (i =11; i <= 14; i++ ){
-
-    //     for (j =11; j <= i; j++){
-    //         cout << j << " ";
-           
+    // Q.4
+    // cout << "Q.4" << endl;
+    // for(int i = 1; i <= 5; i++)
+    // {
+    //     for(int k = 5; k > i; k--)
+    //     {
+    //         cout << " ";
     //     }
-         
+    //     for(int j = 1; j <= i; j++)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
     // }
+
+    // Q.5
+    cout << "Q.5" << endl;
+    for(int i = 5; i >= 1; i--)
+    {
+        for (int j = i; j <= 5; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    // Q.6
+    cout << "Q.6" << endl;
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int k = 5; k > i; k--)
+        {
+            cout << "  ";
+        }
+        for(int j = i; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    //Q.7
+    cout << "Q.7" << endl;
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j <= (2 * 5 - 1); j++)
+        {
+            if(j == 5 - i + 1 || j == 5 + i - 1 || i == 5)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+   }
     
-
-
-{
-    //  Question no .3
-    for (int i = 5; i >= 1  ; i--)
-    {     
-         for (int k = 5; k > i; k--)
-         {
-            cout << "";
-         }
-         for (int j = 1; j <= i; j++)
-         {
-            if(j % 2 == 1){
-                cout << " 1 ";
-            }
-            else{
-                cout << " 0 ";
-            }
-         }
-         cout << endl;    
-    }
-}
-
-{
-//   Question no. 04
-     for ( int i = 5; i >1 ; i--)
-     {
-       for (int  k = 5; k >1; k--)
-       {
-        cout << " ";
-       }
-       for (int j = 1; j <= i ; j++)
-       {
-         cout << j << " ";
-       }
-       cout << endl;   
-     }
-}
-
-
-    // Question no .5
-
-{
-      for ( int i = 5; i > 1 ; i--)
-     {
-       for (int j = 1; j <= i ; j++)
-       {
-         cout << j << " ";
-       }
-       cout << endl; 
-     }
-}
-
-
-//  Question No. 06
-for(int i = 1; i <= 5; i++){
-
-    for (int k= 5; k > i; k--){
-        cout << "  "; 
-    }
-    for (int j = i; j >=1 ; j--){
-
-    cout << j << " " ;     
-    }
-cout << endl;
-}
-
-
-//  Question no. 07
-{
-
-for(char i = 1; i <= 5; i++){
-
-    for (char k= 5; k > i; k--){
-        cout << " * "; 
-    }
-    for (char j = i; j >=1 ; j--){
-
-    cout << j << " " ;     
-    }
-cout << endl;
-
-}
+    
+    return 0;
 }
