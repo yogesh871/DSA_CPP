19a0e57d0828fb72749f7fb943b4b120f050ea87
yogesh <yogeshrd1708@gmail.com>
2025-01-01 11:23:56 +0530

    2D_Array 31/12/2024

--- 13_2D_Array_Exersize/2D_Array.cpp
+++ 13_2D_Array_Exersize/2D_Array.cpp
@@ -3,93 +3,178 @@ using namespace std;
 
 int main()
 {
-//   Q.1 
- 
-{
-
-    int r, c;
-
-    cout << "Enter rows size: ";
-    cin >> r;
-
-    cout << "Enter columns size: ";
-    cin >> c;
-
-    int d[r][c];
-
-    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < c; j++)
+        //   Q.1
+        cout << "Average of all element " << endl;
+        int r, c;
+
+        cout << "Enter rows size: ";
+        cin >> r;
+
+        cout << "Enter columns size: ";
+        cin >> c;
+
+        int d[r][c];
+
+        // Input for the 2D array
+        for (int i = 0; i < r; i++)
         {
-            cout << "Enter a[" << i << "][" << j << "]: ";
-            cin >> d[i][j];
+            for (int j = 0; j < c; j++)
+            {
+                cout << "Enter d[" << i << "][" << j << "]: ";
+                cin >> d[i][j];
+            }
+        }
+
+        // Calculating the average of all elements
+        int totalSum = 0, totalCount = r * c;
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                totalSum += d[i][j];
+            }
+        }
+        int average = totalSum / totalCount;
+        cout << "Average of all elements: " << average << endl;
+    }
+    {
+
+        cout << "Addition two array in another array" << endl;
+        //  Q.2
+        int r, c;
+
+        cout << "Enter rows size: ";
+        cin >> r;
+
+        cout << "Enter columns size: ";
+        cin >> c;
+
+        int a[r][c], b[r][c], f[r][c];
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                cout << "Enter a[" << i << "][" << j << "]: ";
+                cin >> a[i][j];
+            }
+        }
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                cout << "Enter b[" << i << "][" << j << "]: ";
+                cin >> b[i][j];
+            }
+        }
+
+        cout << endl
+             << endl
+             << "=============================" << endl
+             << endl;
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                f[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        cout << endl
+             << "Array result:" << endl;
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                cout << f[i][j] << " ";
+            }
+            cout << endl;
         }
     }
 
-   int length = sizeof(d) / sizeof(d[0][0]);
-    for (int i = 0; i < r; i++)
+    // Q - 3
     {
-        int sum = 0; 
-        for (int j = 0; j < c; j++)
+        cout << "print only bountry element " << endl;
+        int r, c;
+
+        cout << "Enter rows size: ";
+        cin >> r;
+
+        cout << "Enter columns size: ";
+        cin >> c;
+
+        int a[r][c];
+
+        // Input for the 2D array
+        for (int i = 0; i < r; i++)
         {
-            sum += d[i][j];
+            for (int j = 0; j < c; j++)
+            {
+                cout << "Enter a[" << i << "][" << j << "]: ";
+                cin >> a[i][j];
+            }
         }
-        int  average = sum / length; 
-        cout << "Average of row : " << average << endl;
-}
-    }
 
-
-    //  Q.2 
-    int r, c;
-
-    cout << "Enter rows size: ";
-    cin >> r;
-
-    cout << "Enter columns size: ";
-    cin >> c;
-
-    int a[r][c], b[r][c], f[r][c];
-
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
+        // Output the required elements
+        for (int i = 0; i < r; i++)
         {
-            cout << "Enter a[" << i << "][" << j << "]: ";
-            cin >> a[i][j];
-        }
-    }
-
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            cout << "Enter b[" << i << "][" << j << "]: ";
-            cin >> b[i][j];
+            for (int j = 0; j < c; j++)
+            {
+                if (i == 0 || i == r - 1 || j == 0 || j == c - 1)
+                {
+                    cout << a[i][j] << " ";
+                }
+                else
+                {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
         }
     }
 
-
-    cout << endl << endl << "=============================" << endl << endl;
-
-    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < c; j++)
+        //  Q - 4
+        cout << "Print Only Daigram Element2 " << endl;
+        int r, c;
+
+        cout << "Enter rows size: ";
+        cin >> r;
+
+        cout << "Enter columns size: ";
+        cin >> c;
+
+        int a[r][c];
+
+        // Input for the 2D array
+        for (int i = 0; i < r; i++)
         {
-            f[i][j] = a[i][j] + b[i][j];
+            for (int j = 0; j < c; j++)
+            {
+                cout << "Enter a[" << i << "][" << j << "]: ";
+                cin >> a[i][j];
+            }
+        }
+
+        // Output the required elements
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (i == j)
+                {
+                    cout << a[i][j] << " ";
+                }
+                else
+                {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
         }
     }
-
-    cout << endl << "Array result:" << endl;
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            cout << f[i][j] << " ";
-        }
-        cout << endl;
-    }
-
     return 0;
 }
-
---
+++ 13_2D_Array_Exersize/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if (r < 5 || c < 5) {
+    //     cout << "Error: Rows and columns must be at least 5 for this operation." << endl;
+    //     return 1;
+    // }
