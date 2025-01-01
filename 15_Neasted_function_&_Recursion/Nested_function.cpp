8b7fa86d8029186887da50f2f08692f97fa43887
yogesh <yogeshrd1708@gmail.com>
2025-01-01 18:24:25 +0530

    Nested-function & Recurstion 01_01_2025

---
+++ 15_Neasted_function_&_Recursion/Nested_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+    void jenish () {
+        cout << " i am jenish " << endl;
+    }
+
+     void vivek () {
+        cout << " i am vivek" << endl;
+        jenish ();
+     }
+int main() {
+     vivek();
+
+}
---
+++ 15_Neasted_function_&_Recursion/recurstion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//  Factoruial with Recurstion 
+int fact(int n){
+    if(n <= 1){
+      return 1;
+    }
+    else{
+        return  n * fact(n -1);
+    }
+}
+
+int main () {
+    int ans = fact(5);
+    cout << " factorial is :" << ans << endl;
+
+}
