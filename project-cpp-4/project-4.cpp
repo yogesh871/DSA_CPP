d180febe445c21e8ca4f4a71ab2929c123a3979e
yogesh <yogeshrd1708@gmail.com>
2025-01-03 19:02:39 +0530

    project-cpp-4

--- project-cpp-4/project-4.cpp
+++ project-cpp-4/project-4.cpp
@@ -4,63 +4,34 @@ int main()
 
 {
 
-{
-    // Question - 1
-
-
-    int size;
-
-    cout << "Enter array size: ";
-    cin >> size;
-    int a[size];
-    for (int i = 0; i < size; i++)
     {
-        cout << "Enter a value for the array: ";
-        cin >> a[i];
-    }
-    cout << "Array values: ";
-    for (int i = 0; i < size; i++)
-    {
-        if(0 > a[i]){
-        cout << a[i] << " ";
-        }
-        else{
-            cout << " " << endl;
-        }
-    }
-    cout << endl;
-}
+        // Question - 1
+    cout << " Question no .01" << endl;
+        int size;
 
-
-
-
-    {
-    //   Question  - 2
-        int r, c;
-
-        cout << "Enter rows size: ";
-        cin >> r;
-
-        cout << "Enter columns size: ";
-        cin >> c;
-
-        int d[r][c];
-        for (int i = 0; i < r; i++)
+        cout << "Enter array size: ";
+        cin >> size;
+        int a[size];
+        for (int i = 0; i < size; i++)
         {
-            for (int j = 0; j < c; j++)
+            cout << "Enter a value for the array: ";
+            cin >> a[i];
+        }
+        cout << "Array values: ";
+        for (int i = 0; i < size; i++)
+        {
+            if (0 > a[i])
             {
-                cout << "Enter d[" << i << "][" << j << "]: ";
-                cin >> d[i][j];
+                cout << a[i] << " ";
             }
         }
+        cout << endl;
     }
 
-
-
-
-
     {
-    //  Question - 3
+        // Question - 2
+    cout << " Question no .02" << endl;
+
         int r, c;
 
         cout << "Enter rows size: ";
@@ -71,72 +42,125 @@ int main()
 
         int a[r][c];
 
+        for (int i = 0; i < r; ++i)
+        {
+            for (int j = 0; j < c; ++j)
+            {
+                cout << "Enter a[" << i << "][" << j << "]: ";
+                cin >> a[i][j];
+            }
+        }
+      
+    int largest = a[0][0]; 
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (a[i][j] > largest) {
+                largest = a[i][j];
+            }
+        }
+    }
+    cout << "The largest element is: " << largest << endl;
+
+    }
+
+    {
+
+        //  Question - 3
+    cout << " Question no .03" << endl;
+
+        int r, c;
+
+        cout << "Enter rows size: ";
+        cin >> r;
+
+        cout << "Enter columns size: ";
+        cin >> c;
+
+        int a[r][c];
+
+        int **array = new int *[r];
+        for (int i = 0; i < r; i++)
+        {
+            array[i] = new int[c];
+        }
+
+        int **transpose = new int *[c];
+        for (int i = 0; i < c; i++)
+        {
+            transpose[i] = new int[r];
+        }
+
+        cout << "Enter array's elements:\n";
         for (int i = 0; i < r; i++)
         {
             for (int j = 0; j < c; j++)
+            {
+                cout << "a[" << i << "][" << j << "] = ";
+                cin >> array[i][j];
+            }
+        }
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                transpose[j][i] = array[i][j];
+            }
+        }
+        cout << "The transpose matrix of the array:\n";
+        for (int i = 0; i < c; i++)
+        {
+            for (int j = 0; j < r; j++)
+            {
+                cout << transpose[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    {
+        //  Question  - 4
+    cout << " Question no .04" << endl;
+        int r, c;
+
+        cout << "Enter rows size: ";
+        cin >> r;
+
+        cout << "Enter columns size: ";
+        cin >> c;
+
+        int a[r][c];
+
+        for (int i = 0; i < r; ++i)
+        {
+            for (int j = 0; j < c; ++j)
             {
                 cout << "Enter a[" << i << "][" << j << "]: ";
                 cin >> a[i][j];
             }
         }
 
-        for (int i = 0; i < r; i++)
-        {
-            for (int j = 0; j < c; j++)
-            {
-                cout << a[i][j] << " " << endl;
-                ;
-            }
-        }
-    }
-
-
-
-
-    {
-    //  Question  - 4
-
-    int r, c;
-
-    cout << "Enter rows size: ";
-    cin >> r;
-
-    cout << "Enter columns size: ";
-    cin >> c;
-
-    int a[r][c];
-
-    for (int i = 0; i < r; ++i)
-    {
-        for (int j = 0; j < c; ++j)
-        {
-            cout << "Enter a[" << i << "][" << j << "]: ";
-            cin >> a[i][j];
-        }
-    }
-
-    cout << "Row-wise addition:" << endl;
-    for (int i = 0; i < r; ++i)
-    {
-        int rowSum = 0;
-        for (int j = 0; j < c; ++j)
-        {
-            rowSum += a[i][j];
-        }
-        cout << "Row " << i + 1 << ": " << rowSum << endl;
-    }
-
-    cout << "Column-wise addition:" << endl;
-    for (int j = 0; j < c; ++j)
-    {
-        int colSum = 0;
+        cout << "Row-wise addition:" << endl;
         for (int i = 0; i < r; ++i)
         {
-            colSum += a[i][j];
+            int rowSum = 0;
+            for (int j = 0; j < c; ++j)
+            {
+                rowSum += a[i][j];
+            }
+            cout << "Row " << i + 1 << ": " << rowSum << endl;
         }
-        cout << "Column " << j + 1 << ": " << colSum << endl;
-    }
-    }
 
-return 0;
+        cout << "Column-wise addition:" << endl;
+        for (int j = 0; j < c; ++j)
+        {
+            int colSum = 0;
+            for (int i = 0; i < r; ++i)
+            {
+                colSum += a[i][j];
+            }
+            cout << "Column " << j + 1 << ": " << colSum << endl;
+        }
+    }
+    return 0;
 }
---
+++ project-cpp-4/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+  for (int i = 0; i < r; ++i)
+    {
+        for (int j = 0; j < c; ++j)
+        {
+            cout << "Enter a[" << i << "][" << j << "]: ";
+            cin >> a[i][j];
+        }
