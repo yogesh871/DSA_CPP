84f0273f99d950c942378daafbc1e7866270713d
yogesh <yogeshrd1708@gmail.com>
2025-02-10 11:07:51 +0530

    project_cpp_8   08/02/2025

--- Project-cpp-8/project-8-.cpp
+++ Project-cpp-8/project-8-.cpp
@@ -1,112 +1,179 @@
+
 #include <iostream>
 using namespace std;
 
-struct Node
-{
+class Node {
+public:
     int data;
-    Node *next;
+    Node* next;
+    
+   
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
 };
 
-class SinglyLinkedList
-{
+class DynamicMemoryAllocation {
 private:
-    Node *head;
-    int size;
+    Node* head;
 
 public:
-    SinglyLinkedList() : head(nullptr), size(0) {}
-
-    void insertData(int value)
-
-    {
-        Node *newNode = new Node{value, head};
-        head = newNode;
-        size++;
-        cout << "Node inserted at the beginning successfully!" << endl;
+   
+    DynamicMemoryAllocation() {
+        head = nullptr;
     }
 
-    void SerchData(int value)
-    {
-        Node *newNode = new Node{value, head};
-        head = newNode;
-        size--;
-        cout << " Node is found successfully !" << endl;
-    };
+   
+    void insert_at_beginning(int data) {
+        Node* new_node = new Node(data); 
+        new_node->next = head;           
+        head = new_node;                
+        cout << "Node with value " << data << " inserted at the beginning." << endl;
+    }
 
-    void deleteData()
-    {
-        if (head == nullptr)
-        {
-            cout << "List is empty.\n";
+    
+    bool search(int key) {
+        Node* temp = head; 
+        while (temp != nullptr) {
+            if (temp->data == key) {
+                return true; 
+            }
+            temp = temp->next; 
+        }
+        return false; 
+    }
+
+    
+    void delete_node(int key) {
+        if (head == nullptr) {
+            cout << "List is empty. Nothing to delete." << endl;
             return;
         }
-        Node *temp = head;
-        head = head->next;
-        delete temp;
-        size--;
-        cout << "Node deleted from the Memory successfully!" << endl;
-    }
-
-    void Reverse() {
-
-    };
-};
-
-int main()
-{
-
-    int choice, element, id, value;
-
-    do
-    {
-
-        cout << endl
-             << "Dyanamic Memory Allocation " << endl
-             << endl;
-        cout << "Press 1 for Insert new node in beggining" << endl;
-        cout << "Press 2 for serch node " << endl;
-        cout << "Press 3 for Delete node" << endl;
-        cout << "Press 4 for list been reverse oder" << endl;
-        cout << "Press 0 for Exit " << endl;
-
-        cout << "Enter Your Choice : ";
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-            cout << "Enter your element : ";
-            cin >> element;
-            //    insertData();
-            break;
-
-        case 2:
-            cout << "Enter your element : ";
-            cin >> id;
-            cout << "Element value is : " << id << endl;
-            // SerchData();
-
-            break;
-
-        case 3:
-            cout << "you choice delete elememt value: " << endl;
-            cin >> value;
-
-            cout << "deleted value is: " << value << endl;
-            break;
-
-        case 4:
-            break;
-
-        case 0:
-            break;
-
-        default:
-
-            cout << "Plase try again ! Invelid Choice " << endl;
-
-            break;
+        
+         
+        if (head->data == key) {
+            Node* temp = head;
+            head = head->next;  
+            delete temp;        
+            cout << "Node with value " << key << " deleted." << endl;
+            return;
         }
 
-    } while (choice != 0);
+        Node* temp = head;
+     
+        while (temp->next != nullptr && temp->next->data != key) {
+            temp = temp->next;
+        }
+
+          if (temp->next != nullptr) {
+            Node* node_to_delete = temp->next;
+            temp->next = temp->next->next; 
+            delete node_to_delete;         
+            cout << "Node with value " << key << " deleted." << endl;
+        } else {
+            cout << "Node with value " << key << " not found." << endl;
+        }
+    }
+
+   
+    void reverse() {
+        Node* prev = nullptr;
+        Node* current = head;
+        Node* next_node = nullptr;
+
+        while (current != nullptr) {
+            next_node = current->next; 
+            current->next = prev;      
+            prev = current;            
+            current = next_node;
+        }
+        head = prev; 
+        cout << "List reversed." << endl;
+    }
+
+    
+    void display() {
+        if (head == nullptr) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << "nullptr" << endl; 
+    }
+
+
+    ~DynamicMemoryAllocation() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    DynamicMemoryAllocation list;
+    int choice, value;
+
+    do {
+
+        cout << "\nMenu:" << endl;
+        cout << "1. Insert at Beginning" << endl;
+        cout << "2. Search for an Element" << endl;
+        cout << "3. Delete a Node" << endl;
+        cout << "4. Reverse the List" << endl;
+        cout << "5. Display the List" << endl;
+        cout << "6. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: 
+                cout << "Enter value to insert at the beginning: ";
+                cin >> value;
+                list.insert_at_beginning(value);
+                break;
+
+            case 2: 
+                cout << "Enter value to search: ";
+                cin >> value;
+                if (list.search(value)) {
+                    cout << "Element " << value << " found in the list." << endl;
+                } else {
+                    cout << "Element " << value << " not found in the list." << endl;
+                }
+                break;
+
+            case 3: 
+                cout << "Enter value to delete: ";
+                cin >> value;
+                list.delete_node(value);
+                break;
+
+            case 4: 
+                list.reverse();
+                break;
+
+            case 5:
+                cout << "Current List: ";
+                list.display();
+                break;
+
+            case 6: 
+                cout << "Exiting..." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 6);
+
+    return 0;
 }
